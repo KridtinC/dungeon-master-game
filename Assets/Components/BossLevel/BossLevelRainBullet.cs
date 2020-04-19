7a5b2b191a09f0e74ae77e98ec2469cdc4b8b2d4
phirasit <phirasitsharp@gmail.com>
2020-04-19 12:43:44 +0700

    Add phase 2 and phase 3

---
+++ Assets/Components/BossLevel/BossLevelRainBullet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossLevelRainBullet : BossLevelBullet
+{
+    public override void Generate() {
+        direction = -gameObject.transform.up;
+    }
+}
