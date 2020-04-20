e9b329d1223f4450cd188ff48473fbf127bbc85f
Kantanat Siripipatworakun <phantomktn@gmail.com>
2020-04-21 02:39:55 +0700

    add timeScene

--- Assets/Components/TimedPuzzle/TimeController.cs
+++ Assets/Components/TimedPuzzle/TimeController.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Text TimeLeft;
+	public float time = 60f;
+	public bool timeRun = false;
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		timing();
+		TimeLeft.text = time.ToString("f1");
+	}
+
+	void TimeCount(){
+		time -= Time.deltaTime;
+	}
+	float TimeStop(){
+		return time;
+	}
+	private void timing(){
+		if (Input.GetKeyDown(KeyCode.F)){
+					// Puzzle.Activate
+				timeRun = !timeRun;
+				}
+		if(timeRun){
+			TimeCount();
+			}
+		else{
+			TimeStop();
+			}
+	}
 }
